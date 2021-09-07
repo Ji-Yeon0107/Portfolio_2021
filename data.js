da2d69a724a02a3b880d508c10df11dcf6e324e6
JIYEON_OnTime <54522206+Ji-Yeon0107@users.noreply.github.com>
2021-09-08 01:50:40 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+let data = [
+    {
+        title: '오리온 회사 사이트',
+        description: '웹사이트 리뉴얼',
+        skills: 'HTML, CSS, Javascript, Jquery'
+    },
+    {
+        title: '쇼핑몰',
+        description: '일반적인 쇼핑몰 사이트',
+        skills: 'HTML, CSS, React, Javascript'
+    },
+    {
+        title: '오리온 회사 사이트',
+        description: '웹사이트 리뉴얼',
+        skills: 'HTML, CSS, Javascript, Jquery'
+    },
+]
---
+++ index.js
@@ -0,0 +1,17 @@
+// window.onload= function() {
+    
+// let list = document.querySelector(".project-list")
+// let i = 0
+// let j = 0
+// for(i=0; i<3; i++){
+//     let li = document.createElement("li");  
+//     list.append(li);
+//     for(j=0; j<1; j++) {
+//         let div = document.createElement("div");
+//         li.append(div);
+//         div.append("제목입니다@@.");
+//     }
+// }
+
+
+// }
